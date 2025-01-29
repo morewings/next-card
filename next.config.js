78d1cbccff2115d67481941414ee41feca098173
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2025-01-29 19:25:12 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/* Enable bundle analysis. Run `yarn analyze:build` to get report */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+module.exports = withBundleAnalyzer({
+    reactStrictMode: true,
+    distDir: 'build',
+    output: process.env.PAGES_BUILD === 'true' ? 'export' : undefined,
+    cleanDistDir: true,
+    basePath: process.env.PAGES_BUILD === 'true' ? '/ts-global-state-next' : undefined,
+});
