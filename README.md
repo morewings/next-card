fe5bf909acf63d090fd75e6e88c2a740e47b9f7d
Dima Vyshniakov <morewings@gmail.com>
2025-02-02 17:01:15 +0100

    Add Rubik font

--- README.md
+++ README.md
@@ -127,5 +127,19 @@ You can [set up a custom domain](https://docs.github.com/en/pages/configuring-a-
 
 Don't forget to remove `basePath` property from the `./next.config.js`.
 
+## Font support
+
+Next card uses [Rubik font](https://fonts.google.com/specimen/Rubik). By default, it bundles with `latin` and `cyrillic` subsets. You can enable additional subsets at `app/layout.tsx`.
+
+```ts
+import {Rubik} from 'next/font/google';
+
+const rubik = Rubik({
+    subsets: ['latin', 'cyrillic'], // <==
+    display: 'swap',
+});
+
+```
+
 
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type {ReactNode} from 'react';
 import {GoogleAnalytics} from '@next/third-parties/google';
+import {Rubik} from 'next/font/google';
 
-import './index.css';
+import 'the-new-css-reset/css/reset.css';
 import {config} from '@/config';
+
+const rubik = Rubik({
+    subsets: ['latin', 'cyrillic'],
+    display: 'swap',
+});
 
 type Props = {
     readonly children: ReactNode;
@@ -10,7 +16,7 @@ type Props = {
 
 export default function RootLayout({children}: Props) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning className={rubik.className}>
             <head>
                 <meta name="apple-mobile-web-app-title" content="MyWebSite" />
                 <title>{config.title}</title>
