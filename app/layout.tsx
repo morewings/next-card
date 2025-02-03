ebce809028665dbeeed089b14318cf37d21137f9
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2025-02-04 00:30:06 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({children}: Props) {
         <html lang="en" suppressHydrationWarning className={rubik.className}>
             <head>
                 <meta name="apple-mobile-web-app-title" content="MyWebSite" />
-                <meta 
-                    name="description" 
-                    content={`${config.title}: ${config.bio}`} />
+                <meta name="description" content={`${config.title}: ${config.bio}`} />
                 <title>{config.title}</title>
             </head>
             <body>{children}</body>
