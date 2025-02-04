b2a8c2b4c342bb693096d43947737d2467be57af
Dima Vyshniakov <morewings@gmail.com>
2025-02-04 15:50:19 +0100

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({children}: Props) {
     return (
         <html lang="en" suppressHydrationWarning className={rubik.className}>
             <head>
-                <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+                <meta name="apple-mobile-web-app-title" content={config.title} />
+                <meta name="og:title" content={config.title} />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content={config.cardImage?.src} />
+                <meta name="og:description" content={`${config.title}: ${config.bio}`} />
                 <meta name="description" content={`${config.title}: ${config.bio}`} />
                 <title>{config.title}</title>
             </head>
