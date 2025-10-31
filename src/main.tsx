808faa0e3a924f66f369d03af4a9ebda719d5f41
Dima Vyshniakov <morewings@gmail.com>
2025-11-01 00:09:57 +0100

    Clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactGA from 'react-ga4';
 import {ViteReactSSG} from 'vite-react-ssg/single-page';
 
 import App from '@/components/App/App.tsx';
 import 'the-new-css-reset/css/reset.css';
 import './font.css';
-import {config} from '@/config.ts';
-
-if (config.gaId) {
-    ReactGA.initialize(config.gaId);
-}
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
 
 export const createRoot = ViteReactSSG(
     <React.StrictMode>
